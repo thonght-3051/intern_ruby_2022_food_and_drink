346377df24d0bde547a1b2f925496cd556c17a1d
thonght-3051 <hoang.trung.thong@sun-asterisk.com>
2022-08-11 15:22:50 +0700

    Task 51854 :[User] Show list product

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class DashboardController < ApplicationController
   layout "dashboard"
   def index
+    @pagy, @products = pagy Product.latest_product,
+                            items: Settings.products.item_product
+    @categories = Category.all
+    @sizes = Size.all
     render "pages/products_landing"
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@ class Product < ApplicationRecord
   has_many :votes, dependent: :destroy
   has_many :order_details, dependent: :destroy
   has_many :product_attributes, dependent: :destroy
+  has_many :product_images, dependent: :destroy
+
+  scope :latest_product, ->{order created_at: :desc}
+  delegate :name, to: :category, prefix: true
 end
