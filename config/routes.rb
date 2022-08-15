085bef72ff1d4cd0457c9f9212bdbb7e4c2892e7
thonght-3051 <hoang.trung.thong@sun-asterisk.com>
2022-08-15 15:31:28 +0700

    Theme user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'home/index'
+  scope "(:locale)", locale: /en|vi/ do
+    namespace :admin do
+      get 'home/index'
+    end
+
+    get "dashboard", to: "dashboard#index"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
